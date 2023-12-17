4863b2493dd7584d0f52d75b4f0e10befcd719e4
Victoria Purcaro <mv.purcaro@gmail.com>
2023-12-17 14:42:23 -0300

    paciente puede ver sus turnos - falta agregar dentro del card la especialidad a la que se atiendae

--- src/views/ListadoTurnosView.vue
+++ src/views/ListadoTurnosView.vue
@@ -1,14 +1,35 @@
 <script setup>
-import { ref } from "vue";
-import { useUserStore } from "../stores/user";
-import { useRouter, RouterLink } from "vue-router";
-const user = useUserStore();
-const router = useRouter();
+import { ref, onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '../stores/user';
+import { useTurnoStore } from '../stores/turnos';
+import { useRouter, RouterLink } from 'vue-router';
 
-const estasSeguro = (id) => {
-  if (confirm("Estás seguro de que querés eliminar el turno?")) {
-    user.eliminarTurno(id);
+const { usuario } = storeToRefs(useUserStore());
+const medicoNombres = ref({});
+
+onMounted(async () => {
+  console.log('Valor de usuario en Turnos :', usuario.value);
+
+  for (const turno of usuario.value.turnos) {
+    await obtenerMedico(turno.id_medico);
   }
+});
+
+const obtenerMedico = async (id_medico) => {
+  try {
+    const medico = await useUserStore().getUsuarioPorId(id_medico);
+    medicoNombres.value[id_medico] = medico
+      ? `${medico.nombre} ${medico.apellido}`
+      : 'Médico no encontrado';
+  } catch (error) {
+    console.error('Error al obtener médico:', error);
+    medicoNombres.value[id_medico] = 'Error al obtener médico';
+  }
+};
+
+const obtenerNombrePorId = (id_medico) => {
+  return medicoNombres.value[id_medico] || 'Nombre no encontrado';
 };
 </script>
 
@@ -16,26 +37,25 @@ const estasSeguro = (id) => {
   <main>
     <div class="text-center" style="height: fit-content;">
       <h1>
-        Estos son tus turnos, {{ user.nombreUsuario }}
-        {{ user.apellidoUsuario }}
+        Estos son tus turnos, {{ usuario.nombre }}
+        {{ usuario.apellido }}
       </h1>
       <div class="grilla">
-        <div v-for="turno in user.listaTurnos">
+        <div v-for="turno in usuario.turnos">
           <div class="card" style="width: 18rem">
             <div class="card-body">
-              <h5 class="card-title">{{ turno.fecha }}/{{ turno.hora }}</h5>
+              <h5 class="card-title">{{ turno.fechaHora }}</h5>
               <h6 class="card-subtitle mb-2 text-muted">
-                {{ turno.paciente.nombre }} {{ turno.paciente.apellido }}
+               especialidad
               </h6>
               <p class="card-text">
-                Turno con el/la Dr/Dra {{ turno.medico.apellido }}
-                {{ turno.medico.nombre }}
+                Turno con el/la Dr/Dra {{ obtenerNombrePorId(turno.id_medico) }}
               </p>
               <button
                 @click="estasSeguro(turno.id)"
                 class="btn btn-danger card-link"
               >
-                Eliminar
+                Cancelar
               </button>
             </div>
           </div>
